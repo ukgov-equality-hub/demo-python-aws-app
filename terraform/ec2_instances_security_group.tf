f93f600853078e188f56f4ca37764895faac5139
James Griffiths <james@jwgsoftware.com>
2022-12-08 23:46:47 +0000

    Restrict security group access for EC2 instances

--- terraform/ec2_instances_security_group.tf
+++ terraform/ec2_instances_security_group.tf
@@ -12,31 +12,11 @@ resource "aws_security_group" "security_group_main_app_instances" {
 resource "aws_security_group_rule" "security_group_main_app_load_balancer__ingress_port8000_load_balancer" {
   security_group_id = aws_security_group.security_group_main_app_instances.id
   type              = "ingress"
-  description       = "Allow ingress: Port 8000 from Load Balancer"
+  description       = "Allow ingress: Port 80 from Load Balancer"
   protocol          = "tcp"
   from_port         = 8000
   to_port           = 8000
   source_security_group_id = aws_security_group.security_group_main_app_load_balancer.id
-}
-
-resource "aws_security_group_rule" "security_group_main_app_load_balancer__egress_port8000_load_balancer" {
-  security_group_id = aws_security_group.security_group_main_app_instances.id
-  type              = "egress"
-  description       = "Allow egress: Port 8000 to Load Balancer"
-  protocol          = "tcp"
-  from_port         = 8000
-  to_port           = 8000
-  source_security_group_id = aws_security_group.security_group_main_app_load_balancer.id
-}
-
-resource "aws_security_group_rule" "security_group_main_app_load_balancer__ingress_port8000_all" {
-  security_group_id = aws_security_group.security_group_main_app_instances.id
-  type              = "ingress"
-  description       = "Allow ingress: All"
-  protocol          = "-1"
-  from_port         = 0
-  to_port           = 0
-  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group_rule" "security_group_main_app_load_balancer__egress_port8000_all" {
@@ -48,4 +28,3 @@ resource "aws_security_group_rule" "security_group_main_app_load_balancer__egres
   to_port           = 0
   cidr_blocks       = ["0.0.0.0/0"]
 }
-
